bd102b35caba8eef44d7245d8019cae2967b5808
Shirlene Teles <shiteles@gmail.com>
2022-08-19 12:06:56 -0300

    first commit

---
+++ AvaliacaoEmoji/index.js
@@ -0,0 +1,26 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl = document.querySelectorAll(".fa-regular");
+const coresEstrelas =["red","orange","yellow","MediumSpringGreen","green"];
+
+
+atualizaAvaliacao(0);
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", () => {
+        atualizaAvaliacao(index)
+    });
+});
+
+function atualizaAvaliacao(index){
+    starsEl.forEach((starsEl,idx) => {
+        if(idx < index + 1){
+            starsEl.classList.add("active");
+        }else{
+            starsEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojiEl) => {
+        emojiEl.style.transform = `translateX(-${index * 50}px)`;
+        emojiEl.style.color = coresEstrelas[index]
+    });
+}
